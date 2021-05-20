c3786f7f3baba292d8261591839e030f582e1487
Mauricio Bautista <maufbl10@gmail.com>
2021-05-19 23:50:54 -0500

    Spider CIA completadp

--- intelligence_agency_scrapper/intelligence_agency_scrapper/spiders/cia.py
+++ intelligence_agency_scrapper/intelligence_agency_scrapper/spiders/cia.py
@@ -17,7 +17,19 @@ class SpiderCia(scrapy.Spider):
         links_declasified = response.xpath('//a[starts-with(@href,"collection") and (parent::h3 or parent::h2)]/@href').getall()
         for link in links_declasified:
             print(link)
-            yield response.follow(link, callback = self.parse_link)
+            yield response.follow(link, 
+                                callback = self.parse_link,
+                                cb_kwargs = { 
+                                    'url': response.urljoin(link)
+                                })
 
-    def parse_link(self, response):
-        pass
+
+    def parse_link(self, response, **kwargs):
+        link = kwargs['url']
+        title = response.xpath('//h1[@class="documentFirstHeading"]/text()').get()
+        paragrph = response.xpath('//div[@class="field-item even"]//p[not(@class)]/text()').getall()
+        yield {
+            'url':link,
+            'title':title,
+            'body': paragrph
+        }
